c6167dd60f4f31374f0707cab9332351850650b0
Luis Angel Gutierrez Garzon <37279289+gutti666@users.noreply.github.com>
2024-06-25 14:18:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,42 @@
 window.onload = function () {
   console.log("INIT JS");
-  console.log(window.navigator.oscpu )
-  var response = document.getElementById("response");
 
-  response.innerHTML = `<span> OS CPU ${window.navigator.oscpu}</span>`;
 
   var urlIMage = "";
-  // if (navigator.getUserMedia && navigator.o) {
-  //   navigator.getUserMedia({ video: true, audio: false }, succes, error);
 
-  //   function succes() {
-  //     const player = document.getElementById("player");
-  //     const canvas = document.getElementById("canvas");
-  //     const context = canvas.getContext("2d");
-  //     const captureButton = document.getElementById("capture");
-  //     const constraints = {
-  //       video: true,
-  //     };
-
-  //     captureButton.addEventListener("click", async (e) => {
-  //       context.drawImage(player, 0, 0, canvas.width, canvas.height);
-  //       urlIMage = canvas.toDataURL();
-  //       player.srcObject.getVideoTracks().forEach((track) => track.stop());
-  //     });
-
-  //     // Attach the video stream to the video element and autoplay.
-  //     navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
-  //       player.srcObject = stream;
-  //     });
-  //   }
-  //   function error() {}
-  // }
   var form = document.getElementById("DataToIA");
   var response = document.getElementById("response");
   form.addEventListener("submit", async (event) => {
     event.preventDefault();
+    // if (navigator.getUserMedia && navigator.platform == "Win32") {
+    //   navigator.getUserMedia({ video: true, audio: false }, succes, error);
+
+    //   function succes() {
+    //     const player = document.getElementById("player");
+    //     const canvas = document.getElementById("canvas");
+    //     const context = canvas.getContext("2d");
+    //     const captureButton = document.getElementById("capture");
+    //     const constraints = {
+    //       video: true,
+    //     };
+
+    //     captureButton.addEventListener("click", async (e) => {
+    //       context.drawImage(player, 0, 0, canvas.width, canvas.height);
+    //       urlIMage = canvas.toDataURL();
+    //       player.srcObject.getVideoTracks().forEach((track) => track.stop());
+    //     });
+
+    //     // Attach the video stream to the video element and autoplay.
+    //     navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+    //       player.srcObject = stream;
+    //     });
+    //   }
+    //   function error() {}
+    // } else {
+    // }
     const model = document.getElementById("model").value;
-    if (navigator.getUserMedia) {
-    } else {
-      const image = document.getElementById("camera").files[0];
-    }
+    const image = document.getElementById("camera").files[0];
+
     let params = new URLSearchParams(window.location.search);
     const reader = new FileReader();
 
@@ -49,14 +46,14 @@ window.onload = function () {
         reader.readAsDataURL(image);
       });
 
-      const urlAPI = "https://testing-camera.free.beeceptor.com/data"; // Reemplaza con la URL real de tu API
+      const urlAPI = "http://34.48.164.165:8000/analyze"; // Reemplaza con la URL real de tu API
 
       const respuesta = await fetch(urlAPI, {
         method: "POST",
         body: {
           model,
-          image,
-          useragent,
+          base64_image: imagenBase64,
+          useragent:navigator.userAgent,
           mac: params.get("mac"),
         },
       });
